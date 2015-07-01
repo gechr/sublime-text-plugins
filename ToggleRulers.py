1f6b586e9db81bb024952bba5cd01225a4be3a1d
George Christou <gechrr@gmail.com>
2015-07-01 22:19:11 +0100

    ToggleRulers.py: Initial commit

---
+++ ToggleRulers.py
@@ -0,0 +1,8 @@
+import sublime_plugin
+
+class ToggleRulersCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if self.view.settings().get("rulers") == []:
+            self.view.settings().set("rulers", range(4, 160, 4))
+        else:
+            self.view.settings().set("rulers", [])
