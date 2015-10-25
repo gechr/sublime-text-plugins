44b20e87a4429dfe15d680fdf18d2e60cad17fe5
George Christou <gechrr@gmail.com>
2015-10-25 18:12:35 +0000

    SplitPanes.py: Allow direct switching between layouts

--- SplitPanes.py
+++ SplitPanes.py
@@ -8,37 +8,43 @@ import sublime_plugin
 
 """
 
-LAYOUT_NORMAL = {
-    'cols': [0.0, 1.0],
-    'rows': [0.0, 1.0],
-    'cells': [[0, 0, 1, 1]]
-}
-
-LAYOUT_VERTICAL = {
-    "cols": [0.0, 0.5, 1.0],
-    "rows": [0.0, 1.0],
-    "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
-}
-
-LAYOUT_HORIZONTAL = {
-    "cols": [0.0, 1.0],
-    "rows": [0.0, 0.5, 1.0],
-    "cells": [[0, 0, 1, 1], [0, 1, 1, 2]]
+LAYOUTS = {
+    'vertical': {
+        'cols': [0.0, 0.5, 1.0],
+        'rows': [0.0, 1.0],
+        'cells': [[0, 0, 1, 1], [1, 0, 2, 1]]
+    },
+    'horizontal': {
+        'cols': [0.0, 1.0],
+        'rows': [0.0, 0.5, 1.0],
+        'cells': [[0, 0, 1, 1], [0, 1, 1, 2]]
+    },
+    'default': {
+        'cols': [0.0, 1.0],
+        'rows': [0.0, 1.0],
+        'cells': [[0, 0, 1, 1]]
+    }
 }
 
 class SplitPaneCommand(sublime_plugin.WindowCommand):
+
+    def __init__(self, window):
+        self.window = window
+        self.orientation = 'default'
+
     def run(self, *args, **kwargs):
-        orientation = kwargs['orientation']
         w = self.window
-        if w.num_groups() == 1:
-            if orientation == 'vertical':
-                w.run_command('set_layout', LAYOUT_VERTICAL)
-            elif orientation == 'horizontal':
-                w.run_command('set_layout', LAYOUT_HORIZONTAL)
-            else:
-                raise Exception('SplitPanes: Unknown orientation "%s"' % (orientation,))
+        orientation = kwargs.get('orientation', 'default')
+        if self.orientation == 'default':
+            w.run_command('set_layout', LAYOUTS.get(orientation, LAYOUTS['default']))
             if len(w.views()) > 1:
                 w.focus_group(0)
                 w.run_command('move_to_group', {'group': 1})
+            self.orientation = orientation
+        elif self.orientation != orientation:
+            w.run_command('set_layout', LAYOUTS[orientation])
+            self.orientation = orientation
         else:
-            w.run_command('set_layout', LAYOUT_NORMAL)
+            w.run_command('set_layout', LAYOUTS['default'])
+            w.focus_group(1)
+            self.orientation = 'default'
