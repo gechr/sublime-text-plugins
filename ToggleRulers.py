478564b81a5bb0930b67117df068c0682d80545e
George Christou <gechr@users.noreply.github.com>
2015-11-10 15:58:08 +0000

    ToggleRulers.py: Extend width to 200

--- ToggleRulers.py
+++ ToggleRulers.py
@@ -3,6 +3,6 @@ import sublime_plugin
 class ToggleRulersCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         if self.view.settings().get("rulers") == []:
-            self.view.settings().set("rulers", range(4, 160, 4))
+            self.view.settings().set("rulers", range(4, 200, 4))
         else:
             self.view.settings().set("rulers", [])
