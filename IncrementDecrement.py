1d5e5cc561694a4e6823321e2a9fc0f5a5ae49c8
George Christou <gechrr@gmail.com>
2016-02-24 11:47:40 +0000

    IncrementDecrement.py: Add support for strings

--- IncrementDecrement.py
+++ IncrementDecrement.py
@@ -1,22 +1,28 @@
 import sublime_plugin
 
-class NumberCommand(sublime_plugin.TextCommand):
+class IncrementDecrement(sublime_plugin.TextCommand):
     def run(self, edit):
         selection = self.view.sel()
         for region in selection:
+            old_value, new_value = self.view.substr(region), ''
             try:
-                value = int(self.view.substr(region))
-                self.view.replace(edit, region, str(self.op(value)))
+                old_value = int(old_value)
+                new_value = str(self.op(old_value))
             except ValueError:
-                pass
+                new_chars = []
+                for char in old_value:
+                    new_chars.append(chr(self.op(ord(char))))
+                new_value = ''.join(new_chars)
+            if new_value:
+                self.view.replace(edit, region, new_value)
 
     def is_enabled(self):
         return len(self.view.sel()) > 0
 
-class IncrementCommand(NumberCommand):
+class IncrementCommand(IncrementDecrement):
     def op(self, value):
         return value + 1
 
-class DecrementCommand(NumberCommand):
+class DecrementCommand(IncrementDecrement):
     def op(self, value):
         return value - 1
