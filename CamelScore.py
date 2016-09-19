4af753691a4d1232cb205a9af0fd076d24c6cbbc
George Christou <gechrr@gmail.com>
2016-09-19 13:02:35 +0100

    CamelScore.py: Initial commit

---
+++ CamelScore.py
@@ -0,0 +1,31 @@
+import re
+
+import sublime_plugin
+
+class CamelScoreCommand(sublime_plugin.TextCommand):
+
+    def _is_snake_case(self, word):
+        return '_' in word
+
+    def _is_camel_case(self, word):
+        return re.match(r'([A-Z][a-z0-9]+)+', word)
+
+    def _snake_to_camel(self, word):
+        return ''.join(x.title() for x in word.split('_'))
+
+    def _camel_to_snake(self, word):
+        word = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', word)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', word).lower()
+
+    def run(self, edit):
+        for region in self.view.sel():
+            if region.empty():
+                continue
+            word = self.view.substr(region)
+            if self._is_snake_case(word):
+                word = self._snake_to_camel(word)
+            elif self._is_camel_case(word):
+                word = self._camel_to_snake(word)
+            else:
+                continue
+            self.view.replace(edit, region, word)
